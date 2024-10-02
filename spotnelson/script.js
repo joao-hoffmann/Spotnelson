6418656fe81baf3a0966944cfbc402158fdc247d
joao-hoffmann <162444512+joao-hoffmann@users.noreply.github.com>
2024-10-02 09:54:00 -0300

    Add files via upload

---
+++ spotnelson/script.js
@@ -0,0 +1,4 @@
+document.getElementById('loginButton').addEventListener('click', function() {
+    alert('Redirecionando para a página de login...');
+    // Aqui você pode adicionar a lógica para redirecionar para a página de login
+});
